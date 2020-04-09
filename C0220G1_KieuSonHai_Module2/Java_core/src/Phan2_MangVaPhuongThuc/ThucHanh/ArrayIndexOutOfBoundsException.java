1f0f4b7949bff00418125f110bd9aee01d780829
haidakto321 <kieusonhai07@gmail.com>
2020-04-09 14:42:23 +0700

    update baitap

---
+++ C0220G1_KieuSonHai_Module2/Java_core/src/Phan2_MangVaPhuongThuc/ThucHanh/ArrayIndexOutOfBoundsException.java
@@ -0,0 +1,30 @@
+package Phan2_MangVaPhuongThuc.ThucHanh;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayIndexOutOfBoundsException {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            int[] arr=createRandom();
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("Nhập chỉ số của 1 phần tử bất kì: ");
+            int num=scanner.nextInt();
+            try{
+                System.out.println("Giá trị của phần tử có chỉ số: "+num+" là "+arr[num]);
+            } catch(IndexOutOfBoundsException e) {
+                System.err.println("Chỉ số vượt quá giới hạn của mảng.");
+            }
+        }
+        public static int[] createRandom() {
+            Random rd=new Random();
+            int[] arr=new int[100];
+            System.out.println("Danh sách phần tử của mảng:: ");
+            for(int i=0;i<100;i++) {
+                arr[i]=rd.nextInt(100);
+                System.out.println(arr[i]+" ");
+            }
+            return arr;
+        }
+    }
+
--- C0220G1_KieuSonHai_Module2/Java_core/src/Phan2_MangVaPhuongThuc/ThucHanh/LuyenTapTryCatch.java
+++
@@ -1,7 +0,0 @@
-package Phan2_MangVaPhuongThuc.ThucHanh;
-
-public class LuyenTapTryCatch {
-    public static void main(String[] args) {
-
-    }
-}
